b9045398987ba15b0e5151ea662388a29825330b
srtaayanamy <annafelix448@gmail.com>
2025-08-03 16:56:26 -0300

    feat: adicionando pasta de herança

---
+++ 06-poo/03-herança.py
@@ -0,0 +1,77 @@
+'''Herança simples: classe filha herda só de uma classe pai'''
+
+class Veiculo:
+    def __init__(self, cor, placa, numero_rodas):
+        self.cor = cor
+        self.placa = placa
+        self.numero_rodas = numero_rodas
+
+    def ligar_motor(self):
+        print("Ligando o motor")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
+
+class Motocicleta(Veiculo): #obrigações tbm são herdadas
+    pass
+
+
+class Carro(Veiculo):
+    pass
+
+
+class Caminhao(Veiculo):
+    def __init__(self, cor, placa, numero_rodas, carregado):
+        super().__init__(cor, placa, numero_rodas) #chama a implementação da classe pai
+        self.carregado = carregado
+
+    def esta_carregado(self):
+        print(f"{'Sim' if self.carregado else 'Não'} estou carregado")
+
+
+moto = Motocicleta("preta", "abc-1234", 2)
+carro = Carro("branco", "xde-0098", 4)
+caminhao = Caminhao("roxo", "gfd-8712", 8, True)
+
+print(moto)
+print(carro)
+print(caminhao)
+
+'''Herança múltipla: uma classe filha herda de várias classes pai'''
+
+class Animal:
+    def __init__(self, nro_patas):
+        self.nro_patas = nro_patas
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
+
+class Mamifero(Animal):
+    def __init__(self, cor_pelo, **kw):
+        self.cor_pelo = cor_pelo
+        super().__init__(**kw)
+
+
+class Ave(Animal):
+    def __init__(self, cor_bico, **kw):
+        self.cor_bico = cor_bico
+        super().__init__(**kw)
+
+
+class Gato(Mamifero):
+    pass
+
+
+class Ornitorrinco(Mamifero, Ave):
+    def __init__(self, cor_bico, cor_pelo, nro_patas):
+        super().__init__(cor_pelo=cor_pelo, cor_bico=cor_bico, nro_patas=nro_patas)
+
+
+gato = Gato(nro_patas=4, cor_pelo="Preto")
+print(gato)
+
+ornitorrinco = Ornitorrinco(nro_patas=2, cor_pelo="vermelho", cor_bico="laranja") #quando eu uso **kw eu preciso passar o argumentos nomeados
+print(ornitorrinco)
+
